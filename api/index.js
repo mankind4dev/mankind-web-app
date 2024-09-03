269fe11863c36327f20ac30021c65affa3032480
mankind4dev <ogunmakin120@gmail.com>
2024-09-03 17:05:05 +0100

    create connection with mongo database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("MongoDB is Connected Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 const PORT = 3200;
 
 app.listen(PORT, () => {
-  console.log("Server is running on Port 3200!!");
+  console.log("Server is running on Port 3200 Perfectly ");
 });
