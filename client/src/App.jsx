e93546f0718034196656979dc82b69d17b3d4a06
mankind4dev <ogunmakin120@gmail.com>
2024-09-03 16:40:53 +0100

    Setting up Frontend and Backend

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import {Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Header from './components/Header'
+
+
+function App() {
+ 
+
+   return (
+    <>
+        <Header />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/sign-in' element={<Signin />} />
+        <Route exact path='/sign-up' element={<Signup />} />
+        <Route exact path='/projects' element={<Projects />} />
+        <Route exact path='/dashboard' element={<Dashboard />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
